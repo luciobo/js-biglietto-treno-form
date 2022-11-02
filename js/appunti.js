3a6d507e5fb7ea6df3f64ad295768063d93d792d
luciobo <luciobo@icloud.com>
2022-11-02 15:16:58 +0100

    modifiche

---
+++ js/appunti.js
@@ -0,0 +1,16 @@
+
+const annoNascita = document.getElementById("annograb");
+const annograb = annoNascita.value;
+
+console.log(annograb);
+
+
+const nomeUtente = "mario";
+const cognomeUtente = "rossi";
+const annoAttuale = new Date().getFullYear;
+
+let eta;
+
+eta = annoAttuale - annoNascita;
+
+console.log(eta);
